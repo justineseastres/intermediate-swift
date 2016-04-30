b52fca39f6edb435da162ad3105894a8031bacd0
Brian Hill <brian@lingerhere.org>
2016-04-29 18:53:53 -0700

    Removed some so-far-unused playground templates.

--- 3_5-CollectionTypeWithQueueExample.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-/*:
-## 3.5&mdash;CollectionType with Queue Example
-
-_Acknowledgement: This playground follows and simplifies to an intermediate level Section 3.5 of [Advanced Swift]( https://www.objc.io/books/advanced-swift/ ), by Chris Eidhof and Airspeed Velocity._ The section begins by explaning how CollectionType builds on SequenceType, and then illustrates it by implementing a Queue.
-
-*/
